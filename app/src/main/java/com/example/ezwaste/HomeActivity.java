27b035063e3161d65f711b450056301b24f4e793
RisithPerera <risithperera@eng.pdn.ac.lk>
2019-07-27 12:46:48 +0530

    Map Is successfully Attached into the Application

--- app/src/main/java/com/example/ezwaste/HomeActivity.java
+++ app/src/main/java/com/example/ezwaste/HomeActivity.java
@@ -2,18 +2,31 @@ package com.example.ezwaste;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+
+public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -27,7 +40,7 @@ public class HomeActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item){
         switch(item.getItemId()){
             case R.id.option1:
-                Toast.makeText(this,"Settings", Toast.LENGTH_SHORT).show();
+                openLogInActivity();
                 return true;
             case R.id.option2:
                 openSettingsActivity();
@@ -37,8 +50,30 @@ public class HomeActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        /*
+        // Create a LatLngBounds that includes the city of Adelaide in Australia.
+        LatLngBounds sriLanka = new LatLngBounds(new LatLng(5.872396, 81.987547), new LatLng(9.970554, 79.572412));
+        // Constrain the camera target to the Adelaide bounds.
+        mMap.setLatLngBoundsForCameraTarget(sriLanka);
+        */
+
+        // Add a marker in Sydney and move the camera
+        LatLng kandy = new LatLng(7.265, 80.597);
+        mMap.addMarker(new MarkerOptions().position(kandy).title("Kandy Area"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kandy,15));
+    }
+
     private void openSettingsActivity() {
-         Intent intent = new Intent(this, SettingsActivity.class);
-         startActivity(intent);
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    private void openLogInActivity() {
+        Intent intent = new Intent(this, LogInActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/ezwaste/LogInActivity.java
+++ app/src/main/java/com/example/ezwaste/LogInActivity.java
@@ -2,13 +2,32 @@ package com.example.ezwaste;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_log_in);
+
+        Button button = (Button) findViewById(R.id.logInBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHomeActivity();
+            }
+        });
+
     }
+
+    private void openHomeActivity() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
 }
